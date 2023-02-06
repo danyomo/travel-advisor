19e14129bb6fb2b3d0389496921b05525b45525c
danyomo <59213891+danyomo@users.noreply.github.com>
2023-02-06 03:11:30 +0100

    Update Map.js

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -14,7 +14,7 @@ const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherDat
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        bootstrapURLKeys={{ key: 'AIzaSyCZYuoEXt9KRb_I78zwjT-655iyVpitASI' }}
         defaultCenter={coords}
         center={coords}
         defaultZoom={14}
